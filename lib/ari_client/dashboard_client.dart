cf4fedeae4812f5dd337ccf6056d90b17ff1ce78
unknown <Administrator@IGLINTERNS5.zescocorp.zescoad.zesco.co.zm>
2024-04-30 13:09:14 +0200

    Hello

--- lib/ari_client/dashboard_client.dart
+++ lib/ari_client/dashboard_client.dart
@@ -13,14 +13,26 @@ class DasboardClient {
 
   Future<dynamic> send_cdr(Cdr cdr) async {
     var params = cdr.parse();
+    // var uri = Uri(
+    //     scheme: cdr_records.scheme,
+    //     userInfo: cdr_records.userInfo,
+    //     host: cdr_records.host,
+    //     port: cdr_records.port,
+    //     path: cdr_records.path,
+    //     //Iterable<String>? pathSegments,
+    //     query: cdr_records.query,
+    //     queryParameters: params
+    //     //String? fragment
+    //     );
+
     var uri = Uri(
-        scheme: cdr_records.scheme,
-        userInfo: cdr_records.userInfo,
-        host: cdr_records.host,
-        port: cdr_records.port,
-        path: cdr_records.path,
+        scheme: 'http',
+        //userInfo: cdr_records.userInfo,
+        host: '10.43.0.55',
+        port: 80,
+        path: 'api/cdr/cdr/',
         //Iterable<String>? pathSegments,
-        query: cdr_records.query,
+        //query: cdr_records.query,
         queryParameters: params
         //String? fragment
         );
@@ -42,19 +54,31 @@ class DasboardClient {
 
   Future<dynamic> send_call_records(CallRecording cdr) async {
     var params = cdr.parse();
+    // var uri = Uri(
+    //     scheme: voice_records.scheme,
+    //     userInfo: voice_records.userInfo,
+    //     host: voice_records.host,
+    //     port: voice_records.port,
+    //     path: voice_records.path,
+    //     //Iterable<String>? pathSegments,
+    //     query: voice_records.query,
+    //     queryParameters: params
+    //     //String? fragment
+    //     );
+
     var uri = Uri(
-        scheme: voice_records.scheme,
-        userInfo: voice_records.userInfo,
-        host: voice_records.host,
-        port: voice_records.port,
-        path: voice_records.path,
+        scheme: 'http',
+        //userInfo: cdr_records.userInfo,
+        host: '10.43.0.55',
+        port: 80,
+        path: 'api/recordings',
         //Iterable<String>? pathSegments,
-        query: voice_records.query,
+        //query: cdr_records.query,
         queryParameters: params
         //String? fragment
         );
 
-    print(params);
+    //print(params);
 
     //dsvar uri = Uri.http(baseUrl, '/channels', qParams);
     try {
